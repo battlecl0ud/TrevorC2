70b723d2a782633f7217116803f87b5aa94fe9d6
tmc <1370650+khast3x@users.noreply.github.com>
2018-04-17 19:00:51 +0200

    first commit alpine python3

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# USER_INPUT is the user input field on the portainer template interface
+
+if [ -z "$USER_INPUT" ]
+then
+      
+      python trevorc2_server.py "$@"
+      
+else
+      
+      python trevorc2_server $USER_INPUT
+      
+fi
